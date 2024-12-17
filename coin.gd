6d47992f572c920d4171447d4fefa5230ea471fa
Jonas <git@jonasscheiwiller.ch>
2024-12-17 23:13:04 +0100

    feat: add heads up display and show current coins by linking a node (hud)

--- coin.gd
+++ coin.gd
@@ -1,7 +1,7 @@
 extends Area3D
 
 const ROTATION_SPEED = 2 # Number of degrees of rotation per frame
-var level_1 = preload("res://level_1.tscn")
+@export var hud : CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,6 +23,8 @@ func _on_body_entered(body: Node3D) -> void:
 	Global.coins += 1
 	if Global.coins >= Global.totalCoins:
 		get_tree().change_scene_to_file("res://level_1.tscn")
+	
+	hud.get_node("CoinsLabel").text = str(Global.coins)
 
 # event triggered by the Animation within coin
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
