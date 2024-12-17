1479aace3f161553ad75c4781e8c42d8f2e188bc
Jonas <git@jonasscheiwiller.ch>
2024-12-17 20:34:50 +0100

    feat: add custom controls, add controls to rotate camera, adjust gravity and jumping force and move forward in relation to camera

--- steve.gd
+++ steve.gd
@@ -2,10 +2,17 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 10
 
 
 func _physics_process(delta: float) -> void:
+	# rotate cam
+	if Input.is_action_just_pressed("cam_left"):
+		$Camera_Controller.rotate_y(deg_to_rad(30))
+	
+	if Input.is_action_just_pressed("cam_right"):
+		$Camera_Controller.rotate_y(deg_to_rad(-30))
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -16,8 +23,10 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	
+	# new Vector3 direction, taking user arrows and camera rotation into account
+	var direction = ($Camera_Controller.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
